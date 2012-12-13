9f21a8c7fd0e7e2c7e91f1e1c1fb8c2420ebaa22
Sam Zhao <zhao6518@gmail.com>
2012-12-13 00:11:21 -0800

    Fix for precompile error caused by jquery-1.7 included with Mercury

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,7 @@ Iat222::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Mercury config
+  config.assets.precompile += "jquery-1.7.js"
 end
